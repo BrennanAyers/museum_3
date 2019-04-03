489fab051f705709e4863ebee46f2fe4172d6d5e
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-03 14:17:35 -0600

    Add initial Patron tests

---
+++ test/patron_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+class PatronTest < Minitest::Test
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+  def test_it_has_argument_attributes
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+  end
+  
+end
